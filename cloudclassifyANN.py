a5a143a83eb31dea4d54552cbacdc67924f2f867
sunpatchlbr <89615852+sunpatchlbr@users.noreply.github.com>
2024-07-15 16:40:48 -0500

    messing with parameters and setting up test file for further testing

--- cloudclassifyANN.py
+++ cloudclassifyANN.py
@@ -7,14 +7,13 @@ DATA_PATH = '../../Data/TestPhotos/'
 CLASSES = ['NEG','Sky','Cumulus']
 NUM_CLASSES = 3
 TEST_PATH = '../../Data/TestPhotos/BackgroundTest/TEST.jpg'
-BOW_CLUSTERS = 5
+
+MAX_HEIGHT = 1100
+
 BOW_NUM_TRAINING_SAMPLES_PER_CLASS = 20
 ANN_NUM_TRAINING_SAMPLES_PER_CLASS = 20
 
 FLANN_INDEX_KDTREE = 1
-EPOCHS = 10
-ANN_CONF_THRESHOLD = 0.9
-NMS_OVERLAP_THRESHOLD = 0.1
 
 class CloudClassify(object):
     def __init__(self):
@@ -24,9 +23,22 @@ class CloudClassify(object):
         self._classifier = None
         self._sift = None
         self._flann = None
+
+        self._BOW_CLUSTERS = NUM_CLASSES * 4
         self._bow_kmeans_trainer = None
         self._bow_extractor = None
+        
         self._ann = None
+        
+        self._EPOCHS = 20
+        self._ANN_CONF_THRESHOLD = 0.7
+        self._SKY_THRESH = 0.07
+        self._NEG_THRESH = 0.05
+
+        self._ANN_LAYERS = [self._BOW_CLUSTERS, 64, NUM_CLASSES] # input are bow descriptors, output are classes
+
+        self._NMS_OVERLAP_THRESHOLD = 0.3
+        
         self._sky = None
         self._output = None
         self._READY = False
@@ -36,6 +48,7 @@ class CloudClassify(object):
             print("Couldn't find input image file")
         else:
             self._inputImage = cv.imread(inputFileName)
+            self._inputImage = self.resizeInput(self._inputImage, MAX_HEIGHT)
             self.detect_and_classify(self._inputImage)
 
     def get_path_data(self, data_class, i):
@@ -44,6 +57,20 @@ class CloudClassify(object):
                 
     def train(self):
         self.initialize_classifiers()
+
+    def set_parameters(self, epochs, conf_thresh, sky_conf, neg_conf, nms_thresh):
+        self._EPOCHS = 20
+        self._ANN_CONF_THRESHOLD = 0.7
+        self._SKY_THRESH = 0.07
+        self._NEG_THRESH = 0.05
+
+        self._NMS_OVERLAP_THRESHOLD = 0.3
+
+    def set_architecture(self, clusters, inner_layers):
+        self._BOW_CLUSTERS = clusters
+        inner_layers.append(NUM_CLASSES)
+        self._ANN_LAYERS = (np.array(clusters,int)).extend(inner_layers)
+        
 
     def record(self, sample, classification):
         return (np.array([sample], np.float32),
@@ -67,14 +94,13 @@ class CloudClassify(object):
             for class_name in CLASSES:
                 for i in range(BOW_NUM_TRAINING_SAMPLES_PER_CLASS):
                     path = self.get_path_data(class_name, i+1)
-                    #print("PATH: ", path)
                     self.add_sample(path)
 
             voc = self._bow_kmeans_trainer.cluster()
             self._bow_extractor.setVocabulary(voc)
 
             self._ann = cv.ml.ANN_MLP_create()
-            self._ann.setLayerSizes(np.array([BOW_CLUSTERS,75,NUM_CLASSES])) # input are bow descriptors, output are classes
+            self._ann.setLayerSizes(np.array(ANN_LAYERS))
             self._ann.setActivationFunction(cv.ml.ANN_MLP_SIGMOID_SYM, 0.6, 1.0)
             self._ann.setTrainMethod(cv.ml.ANN_MLP_BACKPROP, 0.1, 0.1)
             self._ann.setTermCriteria((cv.TERM_CRITERIA_MAX_ITER | cv.TERM_CRITERIA_EPS, 100, 1.0))
@@ -124,22 +150,36 @@ class CloudClassify(object):
             pos_rects = []
             for resized in self.pyramid(gray_img):
                 print("resized: ", resized.shape)
-                print("layers: ", self._ann.getLayerSizes())
                 for x, y, roi in self.sliding_window(resized):
                     descriptors = self.extract_bow_descriptors(roi)
                     if descriptors is None:
                         continue
-                    print("desc: ", descriptors[0])
-                    print("desc shape: ", descriptors[0].shape)
                     prediction = self._ann.predict(descriptors)
                     class_id = int(prediction[0])
-                    print("prediction: ", prediction)
+                    confidence = prediction[1][0][class_id]
+                    sky_conf = abs(prediction[1][0][1])
+                    NEG_conf = abs(prediction[1][0][0])
+                    if confidence > ANN_CONF_THRESHOLD and sky_conf < SKY_THRESH and NEG_conf < NEG_THRESH:
+                        h, w = roi.shape
+                        scale = gray_img.shape[0] / \
+                            float(resized.shape[0])
+                        pos_rects.append(
+                            [int(x * scale),
+                             int(y * scale),
+                             int((x+w) * scale),
+                             int((y+h) * scale),
+                             confidence,
+                             sky_conf,
+                             NEG_conf,
+                             class_id])
             pos_rects = nms(np.array(pos_rects), NMS_OVERLAP_THRESHOLD)
             print(pos_rects)
-            for x0, y0, x1, y1, score in pos_rects:
+            for x0, y0, x1, y1, score, sky_conf, NEG_conf, class_id in pos_rects:
                 cv.rectangle(img, (int(x0), int(y0)), (int(x1), int(y1)),
                               (0, 255, 255), 2)
-                text = ('%.2f' % score)
+                text = CLASSES[int(class_id)] + ' ' \
+                    + ('%.2f' % score) + ' ' + ('%.2f' % sky_conf) \
+                    + ' ' + ('%.2f' % NEG_conf)
                 cv.putText(img, text, (int(x0), int(y0) - 20),
                             cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
             cv.imshow(TEST_PATH, img)
@@ -149,7 +189,7 @@ class CloudClassify(object):
             exit(1)
         
 
-    def sliding_window(self, img, step=75, window_size=(300, 200)):
+    def sliding_window(self, img, step=40, window_size=(150, 100)):
         img_h, img_w = img.shape
         window_w, window_h = window_size
         for y in range(0, img_w, step):
@@ -159,8 +199,8 @@ class CloudClassify(object):
                 if roi_w == window_w and roi_h == window_h:
                     yield (x, y, roi)
 
-    def pyramid(self, img, scale_factor=2, min_size=(300, 500),
-                max_size=(3000, 3000)):
+    def pyramid(self, img, scale_factor=1.5, min_size=(300, 300),
+                max_size=(2000, 2000)):
         h, w = img.shape
         min_w, min_h = min_size
         max_w, max_h = max_size
@@ -171,6 +211,12 @@ class CloudClassify(object):
             h /= scale_factor
             img = cv.resize(img, (int(w), int(h)),
                              interpolation=cv.INTER_AREA)
+    def resizeInput(self, img, limit):
+        if (img.shape[0] > limit):
+            factor = float(limit) / float(img.shape[0])
+            factor = factor * factor
+            img = cv.resize(img, (0, 0), fx = factor, fy = factor)
+        return img
 
     def isolate_sky(self, originalImg, fg_proportion=0.4):
         print("Isolating sky from foreground...")
--- tester.py
+++ tester.py
@@ -4,6 +4,29 @@ import os
 import sys
 import cloudclassifyANN as cc
 
+CLUSTERS = 15
+
+EPOCHS = 15
+
+CONF_THRESH = 0.6
+SKY_THRESH = 0.1
+NEG_THRESH = 0.1
+
+NMS_THRESH = 0.3
+
+TEST_LOCATION = '../../Data/TestPhotos/BackgroundTest/'
+TEST_FILES = ['IFAK4250.JPG','TEST.JPG','test6.JPG', 'test11.JPG', 'test12.JPG']
+
 cloud = cc.CloudClassify()
+cloud.set_parameters(
+    epochs = EPOCHS,
+    conf_thresh = CONF_THRESH,
+    sky_conf = SKY_THRESH,
+    neg_conf = NEG_THRESH,
+    nms_thresh = NMS_THRESH)
+cloud.set_architecture(15, [64]) # output layer (num classes) not allowed to change
 cloud.train()
-cloud.run()
+
+for test in TEST_FILES:
+    test_path = TEST_LOCATION + test
+    cloud.run(test_path)
